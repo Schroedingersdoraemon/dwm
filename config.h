6fb3085b2570addf696b52196472e96d36b62eb9
Schroedingersdoraemon <1181822466@qq.com>
2020-06-04 13:07:39 +0800

    default terminal: st

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "run", NULL };
 static const char *jgmenucmd[] = { "jgmenu_run", NULL };
 static const char *webcamcmd[] = { "/home/dylan/scripts/webcam.sh", NULL};
