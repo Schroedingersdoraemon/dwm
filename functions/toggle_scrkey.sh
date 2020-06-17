fcc3f1bcb7c4dd9956fb5a2b6b347e90ac64412f
Schroedingersdoraemon <1181822466@qq.com>
2020-06-18 03:20:23 +0800

    _

--- functions/toggle_scrkey.sh
+++ functions/toggle_scrkey.sh
@@ -2,9 +2,11 @@ RUN="$(pgrep -x screenkey|wc -l)"
 ID="$(pgrep -x screenkey)"
 case $RUN in
     0)
+        notify-send -t 1000 'Screenkey started'
         screenkey
         ;;
     1)
+        notify-send -t 1000 'Screenkey killed'
         kill $ID
         ;;
 esac
