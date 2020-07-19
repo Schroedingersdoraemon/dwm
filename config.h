7909ffb4718a50a5fd34eda5f37eeb99ec2271db
Schroedingersdoraemon <1181822466@qq.com>
2020-07-19 16:23:14 +0800

    optional rofi good

--- config.h
+++ config.h
@@ -93,6 +93,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "run", NULL };
+//static const char *roficmd[]  = { "rofi", "-show", "run", "-config", "~/prog/rofi-spotlight/rofi.rasi", NULL };
+
 
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x25", NULL };
