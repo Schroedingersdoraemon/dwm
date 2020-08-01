a632956157ef30029aba0d72071acb230c7eb3b0
Schroedingersdoraemon <1181822466@qq.com>
2020-08-02 02:25:48 +0800

    added plank and netease music

--- config.h
+++ config.h
@@ -56,8 +56,10 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  switch2tag  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         0,             1,          0,           0,        -1 },
+	{ "Plank",   NULL,     NULL,           1 << 0,    1,             0,          0,          -1,        -1 },
 	{ "firefox", NULL,     NULL,           1 << 1,    1,             0,          0,          -1,        -1 },
 	{ "Brave",   NULL,     NULL,           1 << 1,    1,             0,          0,          -1,        -1 },
+	{ "netease-cloud-music",NULL,NULL,     1 << 3,    1,             0,          0,          -1,        -1 },
 	{ "qv2ray",  NULL,     NULL,           1 << 4,    1,             0,          0,          -1,        -1 },
 	{ "Thunderbird",NULL,  NULL,           1 << 4,    1,             0,          0,          -1,        -1 },
 	{ "SimpleScreenRecorder",NULL, NULL,   1 << 5,    1,             0,          0,          -1,        -1 },
