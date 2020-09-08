7b8c141b65dea1aaa611a75be70671da541235f3
Schroedingersdoraemon <1181822466@qq.com>
2020-09-08 21:29:57 +0800

    firefox -> ie

--- config.h
+++ config.h
@@ -46,8 +46,8 @@ static const unsigned int alphas[][3]      = {
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 //static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
-//icon_lists = {   }
-static const char *tags[] = { "", "", "🪐", "", "", "", "\ue727", "", "" };
+//icon_lists = {  }
+static const char *tags[] = { "", "", "🪐", "", "", "", "\ue727", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
