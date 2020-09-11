31e2f45c8ef6de7ac9a11f6067bddeea49e93d13
Schroedingersdoraemon <1181822466@qq.com>
2020-09-11 20:28:10 +0800

    removed copying emoji to primary

--- functions/emoji.sh
+++ functions/emoji.sh
@@ -7,6 +7,6 @@ emoji=$(grep -v "#" "$file" | dmenu -l 20 -fn SauceCodeProNerdFontMono-18| awk '
 #emoji=$(grep -v "#" "$file" | dmenu -l 20 -fn symbola-18| awk '{print $1}')
 
 echo $emoji | xclip -selection clipboard
-echo $emoji | xclip -selection primary
+#echo $emoji | xclip -selection primary
 
 notify-send "$(xclip -o -selection clipboard) copied to clipboard"
