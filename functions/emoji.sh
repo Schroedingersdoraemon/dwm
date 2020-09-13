00634aaefcf676aa519d301ddb799cc5b10ab9a9
Schroedingersdoraemon <1181822466@qq.com>
2020-09-13 13:46:26 +0800

    -

--- functions/emoji.sh
+++ functions/emoji.sh
@@ -2,9 +2,7 @@
 
 file="/home/dylan/prog/dmenu/emoji.mb"
 
-#emoji=$(grep -v "#" "$file" | dmenu -l 20 -fn Monospace-18| awk '{print $1}')
 emoji=$(grep -v "#" "$file" | dmenu -l 20 -fn SauceCodeProNerdFontMono-18| awk '{print $1}')
-#emoji=$(grep -v "#" "$file" | dmenu -l 20 -fn symbola-18| awk '{print $1}')
 
 echo $emoji | xclip -selection clipboard
 #echo $emoji | xclip -selection primary
