4923a5d6312fff00ba6115ebbf055c4b75652b0e
Schroedingersdoraemon <1181822466@qq.com>
2020-09-27 10:40:47 +0800

    wine refined code

--- functions/desktop.sh
+++ functions/desktop.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-choices="Anaconda Navigator\nQQ\nWeChat\nBaiduNetdisk\nPamac Manager"
+choices="Anaconda Navigator\nQQ\nWeChat\nBaiduNetdisk\nGentoo\nShutdownGentoo"
 
 chosen=$(echo -e $choices | dmenu -p "Enter the application：")
 
 #For special characters, use '\' to transform
 case $chosen in
     QQ)
-        env LANG=zh_CN.UTF-8 wine "/home/dylan/.wine/drive_c/Program Files (x86)/Tencent/QQ/Bin/QQ.exe" ;;
+        env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/QQ/Bin/QQ.exe" ;;
     WeChat)
-        env LANG=zh_CN.UTF-8 wine "/home/dylan/.wine/drive_c/Tencent/WeChat/WeChat.exe" ;;
+        env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe" ;;
     BaiduNetdisk)
         /usr/lib/baidunetdisk/baidunetdisk --no-sandbox %U ;;
     Pamac\ Manager)
         env LANG=zh_CN.UTF-8 pamac-manager;;
     Anaconda\ Navigator)
         /opt/anaconda/bin/anaconda-navigator ;;
+    Gentoo)
+        vboxmanage startvm gentoo ;;
+    ShutdownGentoo)
+        kill $(pidof VirtualBoxVM) ;;
 esac
