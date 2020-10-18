7efad0176cf40037b682cef4521abedfec56f322
Schroedingersdoraemon <1181822466@qq.com>
2020-10-18 19:30:35 +0800

    not swallow zathura for markdown/LaTex previewing

--- config.h
+++ config.h
@@ -56,6 +56,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  switch2tag  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         0,             1,          0,           0,        -1 },
+	{ "Zathura", 	NULL, NULL, 0, 0, 0, 0, 1, -1},
 	{ "Pcmanfm",    NULL,   NULL,   0,  0,  1,  0,  0,  -1 },
 	{ "Nm-connection-editor",    NULL,   NULL,   0,  0,  1,  0,  0,  -1 },
 	{ "Plank",   NULL,     NULL,           1 << 0,    1,             0,          0,          -1,        -1 },
