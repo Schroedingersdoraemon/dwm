2304730903c5949a23989cf4c8d35566d7fae1dd
Schroedingersdoraemon <1181822466@qq.com>
2020-11-04 18:00:25 +0800

    wine god-damn ridiculous-tencent-qq fffff

--- functions/desktop.sh
+++ functions/desktop.sh
@@ -6,16 +6,17 @@ chosen=$(echo -e $choices | dmenu -p "Enter the application：")
 #For special characters, use '\' to transform
 case $chosen in
     QQ)
-        # env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/QQ/Bin/QQ.exe" ;;
-        if [ $(pgrep -x gsd-xsettings | wc -l) -eq 0 ]; then
-            /usr/lib/gsd-xsettings &
-        elif [ $(pgrep -x gsd-xsettings | wc -l) -eq 1 ]; then
-            echo ''
-        fi
+        env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/QQ/Bin/QQ.exe" ;;
+        #if [ $(pgrep -x gsd-xsettings | wc -l) -eq 0 ]; then
+            #/usr/lib/gsd-xsettings &
+        #elif [ $(pgrep -x gsd-xsettings | wc -l) -eq 1 ]; then
+            #echo ''
+        #fi
         #/opt/deepinwine/apps/Deepin-QQ/run.sh ;;
-        /opt/deepinwine/apps/Deepin-TIM/run.sh ;;
+        #/opt/deepinwine/apps/Deepin-TIM/run.sh ;;
     WeChat)
-        env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe" ;;
+        #env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe" ;;
+        env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "~/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe" ;;
     BaiduNetdisk)
         /usr/lib/baidunetdisk/baidunetdisk --no-sandbox %U ;;
     Anaconda\ Navigator)
@@ -28,7 +29,7 @@ case $chosen in
 		~/SteamLibrary/steamapps/common/DDraceNetwork/ddnet/DDNet ;;
         #steam steam://rungameid/412220 ;;
     emu8086)
-        wine ~/.wine/drive_c/emu8086/emu8086.exe ;;
+        WINEARCH=win32 WINEPREFIX=~/.wine32 wine ~/.wine32/drive_c/emu8086/emu8086.exe ;;
 	BurpSuiteCommunity)
 		/opt/BurpSuiteCommunity/BurpSuiteCommunity ;;
 	Davinci\ Resolve)
