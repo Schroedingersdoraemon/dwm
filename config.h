56171f2298d667d37eedab52bbb2a35d769c10e3
Schroedingersdoraemon <1181822466@qq.com>
2021-06-23 19:45:41 +0800

    dmenu desktop entry --> sxhkd

--- config.h
+++ config.h
@@ -126,13 +126,9 @@ static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "80x25", NULL };
 
-// desktop support for dmenu
-static const char *dmenudesktopcmd[] = { "/home/dylan/prog/dwm/functions/desktop.sh" };
-
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ Mod1Mask,                     XK_d,      spawn,          {.v = dmenucmd } },
-    { Mod1Mask,                     XK_a,      spawn,          {.v = dmenudesktopcmd} },
     { Mod1Mask,                     XK_r,      spawn,          SHCMD("rofi -show drun") },
 
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
