21503b40dc37aac096a098e91a78ca2ac6ae8111
Schroedingersdoraemon <1181822466@qq.com>
2021-06-23 19:49:18 +0800

    removed mpv camera cmd -> sxhkd

--- config.h
+++ config.h
@@ -152,9 +152,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 
-    //{ MODKEY,                       XK_w,      spawn,         SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)")},
-    { MODKEY,                       XK_w,      spawn,         SHCMD("mpv av://v4l2:/dev/video0 --profile=low-latency --untimed")},
-
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 
