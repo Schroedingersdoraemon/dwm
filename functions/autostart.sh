4318118edd0c85d32ba56f7c47907fa8c2de6e7b
Schroedingersdoraemon <1181822466@qq.com>
2021-08-03 19:24:25 +0800

    -

--- functions/autostart.sh
+++ functions/autostart.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-~/scripts/backgrounds/backgrounds.sh &
+~/scripts/backgrounds/backgrounds.sh > /dev/null 2>&1 &
 #$HOME/.local/bin/anime_wall.sh > /dev/null 2>&1 &
 
-dwmblocks &
+dwmblocks > /dev/null 2>&1 &
 
-picom &
-flameshot &
-nm-applet &
+picom > /dev/null 2>&1 &
+flameshot > /dev/null 2>&1 &
+nm-applet > /dev/null 2>&1 &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 > /dev/null 2>&1 &
 
-dunst -config ~/.config/dunst/dunstrc_dracula &
+ /usr/lib/polkit-kde-authentication-agent-1 > /dev/null 2>&1  
+#/usr/bin/polkit-dumb-agent > /dev/null 2>&1
 
-sxhkd &
+dunst -config ~/.config/dunst/dunstrc_dracula > /dev/null 2>&1 &
+
+sxhkd > /dev/null 2>&1 &
