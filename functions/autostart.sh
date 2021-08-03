e77a683f0213a401b6af280ff5cef89ccf8b3871
Schroedingersdoraemon <1181822466@qq.com>
2021-08-04 05:00:50 +0800

    -

--- functions/autostart.sh
+++ functions/autostart.sh
@@ -11,9 +11,6 @@ nm-applet > /dev/null 2>&1 &
 
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 > /dev/null 2>&1 &
 
- /usr/lib/polkit-kde-authentication-agent-1 > /dev/null 2>&1  
-#/usr/bin/polkit-dumb-agent > /dev/null 2>&1
-
 dunst -config ~/.config/dunst/dunstrc_dracula > /dev/null 2>&1 &
 
 sxhkd > /dev/null 2>&1 &
