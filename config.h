e0b138f5dc014d086a1869b7814eefd94d7d3428
Schroedingersdoraemon <1181822466@qq.com>
2021-08-04 19:25:31 +0800

    updated new tag icons

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const XPoint stickyiconbb    = {4,4};	/* defines the bottom right corner 
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "", "", "🪐", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "ﳟ", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
