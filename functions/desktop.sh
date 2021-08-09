e2ca947450bcc625d49f9f77952011aa55f205a7
Schroedingersdoraemon <1181822466@qq.com>
2021-08-09 21:04:00 +0800

    ~ -> $HOME

--- functions/desktop.sh
+++ functions/desktop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-choices="BaiduNetdisk\nBurpSuiteCommunity\nRemoveQQCache\nShutdownNow\nGTA_ViceCity\nFortressEscape\nMaimWindow\nGentoo\npvz\npyftpd\nDunst\nNotion\nAnimated_Wallpaper\nHibernate"
+choices="BaiduNetdisk\nBurpSuiteCommunity\nRemoveQQCache\nShutdownNow\nGTA_ViceCity\nFortressEscape\nMaimWindow\nGentoo\npvz\npyftpd\nDunst\nNotion\nAnimated_Wallpaper\nHibernate\nX11VNCServer"
 
 chosen=$(echo -e $choices | dmenu -p "Enter the application：")
 
@@ -7,31 +7,20 @@ chosen=$(echo -e $choices | dmenu -p "Enter the application：")
 case $chosen in
     QQ)
 		/opt/tencent-qq/qq ;;
-        #env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/QQ/Bin/QQ.exe" ;;
-        #if [ $(pgrep -x gsd-xsettings | wc -l) -eq 0 ]; then
-            #/usr/lib/gsd-xsettings &
-        #elif [ $(pgrep -x gsd-xsettings | wc -l) -eq 1 ]; then
-            #echo ''
-        #fi
-        #/opt/deepinwine/apps/Deepin-QQ/run.sh ;;
-        #/opt/deepinwine/apps/Deepin-TIM/run.sh ;;
-    #WeChat)
-        #env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "/home/dylan/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe" ;;
-        #env LANG=zh_CN.UTF-8 WINEDEBUG=-all,+fps wine "~/.wine/drive_c/Program Files/Tencent/WeChat/WeChat.exe" ;;
     BaiduNetdisk)
         /usr/lib/baidunetdisk/baidunetdisk --no-sandbox %U ;;
 	BurpSuiteCommunity)
 		/opt/BurpSuiteCommunity/BurpSuiteCommunity ;;
 	RemoveQQCache)
-		rm -r ~/.config/tencent-qq/ ;;
+		rm -r $HOME/.config/tencent-qq/ ;;
 	ShutdownNow)
 		shutdown now ;;
 	GTA_ViceCity)
-		cd ~/SteamLibrary/steamapps/common/Grand\ Theft\ Auto\ Vice\ City && ./reVC ;;
+		cd $HOME/SteamLibrary/steamapps/common/Grand\ Theft\ Auto\ Vice\ City && ./reVC ;;
 	FortressEscape)
-		~/Games/FortressEscape/FortressEscape.x86 ;;
+		$HOME/Games/FortressEscape/FortressEscape.x86 ;;
 	MaimWindow)
-		maim -s | convert - \( +clone -background black -shadow 80x3+5+5 \) +swap -background none -layers merge +repage ~/Pictures/screenshots/$(date +%F_%H-%M).png ;;
+		maim -s | convert - \( +clone -background black -shadow 80x3+5+5 \) +swap -background none -layers merge +repage $HOME/Pictures/screenshots/$(date +%F_%H-%M).png ;;
 	Gentoo)
 		qemu-system-x86_64\
 			-m 2G \
@@ -46,7 +35,7 @@ case $chosen in
 			> /dev/null 2>&1
 			;;
 	pvz)
-		cd ~/Games/pvz_goty/ && wine ./PlantsVsZombies.exe ;;
+		cd $HOME/Games/pvz_goty/ && wine ./PlantsVsZombies.exe ;;
 	pyftpd)
 		/usr/bin/python $HOME/scripts/ftp/ftp_exec.py ;;
 	Dunst)
@@ -57,5 +46,7 @@ case $chosen in
 		$HOME/scripts/backgrounds/anime_wall.sh > /dev/null 2>&1 & ;;
 	Hibernate)
 		pkexec systemctl hibernate ;;
-		# $HOME/scripts
+	X11VNCServer)
+		x11vnc -rfbauth $HOME/.config/vnc_pass > /dev/null 2>&1 &;;
+
 esac
