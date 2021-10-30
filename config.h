06aa32f61988e05d955306221902f10c8ee53f83
Schroedingersdoraemon <1181822466@qq.com>
2021-10-30 17:46:50 +0800

    format update

--- config.h
+++ config.h
@@ -27,11 +27,11 @@ static const char *fonts[]          = {
 };
 
 /* old dark scheme
-   static const char col_gray1[]       = "#222222";
-   static const char col_gray2[]       = "#444444";
-   static const char col_gray3[]       = "#bbbbbb";
-   static const char col_gray4[]       = "#eeeeee";
-   static const char col_cyan[]        = "#343434";
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#343434";
 //static const char col_cyan[]        = "#37474F";
 */
 
